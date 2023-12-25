fc1aadfbbe2728136322b10c49ea5eb8d63c594b
Zhanna14 <tkachukzhanna25@gmail.com>
2023-12-25 22:37:50 +0100

    kjnv

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,25 +1,25 @@
-new Swiper('.yacht-swiper', {
-
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true
-    },
-    spaceBetween: 32,
-    breakpoints: {
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: false,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true
+  },
+  spaceBetween: 32,
+  breakpoints: {
     768: {
       slidesPerView: 2,
     },
     1280: {
       slidesPerView: 3,
       spaceBetween: 25,
-      },
-      1440: {
+    },
+    1440: {
       slidesPerView: 3,
       spaceBetween: 32,
     },
   },
-  
 });
-
-
-
